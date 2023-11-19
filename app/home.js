3efc336b0d42c37b274cb95e0f52e8fa5a4e18e0
Hunter Wiegand <hunter_wiegand@yahoo.com>
2023-11-19 13:11:40 -0800

    Added blueprint router files

---
+++ app/home.js
@@ -0,0 +1,18 @@
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView, ImageBackground, StyleSheet } from "react-native";
+// Can this on "View" as a <div> and "Text" as <p>
+
+// Local imports
+import { Welcome } from "../components";
+import { images } from "../constants";
+
+const Home = () => {
+    const router = useRouter();
+
+    return (
+        <Text>HOME PAGE</Text>
+    )
+};
+
+export default Home;
