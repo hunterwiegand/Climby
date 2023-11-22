b4eeb497ff919e913e9c1630866f29c76d99e4fe
Hunter Wiegand <hunter_wiegand@yahoo.com>
2023-11-21 18:01:31 -0800

    added example climb data

--- database/example.js
+++ database/example.js
@@ -1,5 +1,13 @@
-const data = () => {
-
+module.exports = class Climb {
+    constructor( name, grade, style, date, description) {
+        this.name = name;
+        this.grade = grade;
+        this.style = style;
+        this.date = date;
+        this.description = description;
+    };
 }
 
-export default data;
+//const exampleClimb = new Climb("Sparky", 4, "vert", "10/15/23", "This was my very first dyno");
+
+//console.log(exampleClimb.name);
