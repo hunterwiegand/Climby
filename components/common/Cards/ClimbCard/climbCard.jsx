77faa05f2958f8200eeb24641334c20b50375f1e
Kurtis <kcd257@yahoo.com>
2023-11-21 19:21:45 -0800

    added card data

--- components/common/Cards/ClimbCard/climbCard.jsx
+++ components/common/Cards/ClimbCard/climbCard.jsx
@@ -1,12 +1,19 @@
 import { View, Text, TouchableOpacity } from "react-native";
 
 import styles from "./climbcard.style";
+import Climb from "../../../../database/example";
+
+const test = new Climb("Sparky", 4, "vert", "10/15/23", "This was my very first dyno")
 
 const climbCard = () => {
   return (
     <View style={styles.container}>
-        <TouchableOpacity onPress={() => alert("It's the climb - Miley Cyrus")}>
-            <Text style={styles.text}>Climb Card</Text>
+        <TouchableOpacity style={styles.textContainer} onPress={() => alert("'It's the climb' - Miley Cyrus")}>
+                <Text style={styles.text}>{test.name}</Text>
+                <Text style={styles.text}>{test.grade}</Text>
+                <Text style={styles.text}>{test.style}</Text>
+                <Text style={styles.text}>{test.date}</Text>
+                <Text style={styles.text}>{test.description}</Text>
         </TouchableOpacity>
     </View>
   )
--- components/common/Cards/ClimbCard/climbcard.style.js
+++ components/common/Cards/ClimbCard/climbcard.style.js
@@ -3,18 +3,22 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "space-between",
         alignItems: "center",
         flexDirection: "row",
         padding: 16,
         borderRadius: 12,
-
-    },
+        backgroundColor: "#FFF",
+      },
     text: {
         textAlign: 'center',
-        backgroundColor: 'green',
+        backgroundColor: 'black',
         color: 'white',
     },
+    textContainer: {
+        flex: 1,
+        marginHorizontal: 16,
+      },
 })
 
 export default styles;
