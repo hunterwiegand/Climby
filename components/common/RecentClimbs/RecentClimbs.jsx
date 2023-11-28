bdb049dd52d3ee82b74aa5cea58832b08c7b0add
Hunter Wiegand <hunter.wiegand@docusign.com>
2023-11-28 13:02:46 -0800

    Fixed display issue with ScrollView in home page

--- components/common/RecentClimbs/RecentClimbs.jsx
+++ components/common/RecentClimbs/RecentClimbs.jsx
@@ -13,47 +13,52 @@ const RecentCLimbs = () => {
     const [file, setFile] = useState([]);
 
     // Uncomment to render previous climbs
-    React.useEffect(() => {
-        //Get climb filenames from db
-        listFiles().then((listResponse) => {
+    // React.useEffect(() => {
+    //     //Get climb filenames from db
+    //     listFiles().then((listResponse) => {
 
-            const files = listResponse.map((value) => {
-                return { name: value.fullPath }
-            })
+    //         const files = listResponse.map((value) => {
+    //             return { name: value.fullPath }
+    //         })
 
-            setFile(files);
-        });
-    }, []);
+    //         setFile(files);
+    //     });
+    // }, []);
 
     //get climb Metadata from db
-    // React.useEffect(() => {
+    React.useEffect(() => {
 
-    //     // Get climb filenames from db
-    //     // listMetadata("climbs/30fd123d-ec54-4bfb-8d4e-7ff3995a6b76.mp4").then((metadataResponse) => {
-    //     //     data = [{
-    //     //         file: metadataResponse.file,
-    //     //         name: metadataResponse.name,
-    //     //         grade: metadataResponse.grade
-    //     //     }]
-    //     //     setFile(data);
-    //     // });
+        // Get climb filenames from db
+        // listMetadata("climbs/30fd123d-ec54-4bfb-8d4e-7ff3995a6b76.mp4").then((metadataResponse) => {
+        //     data = [{
+        //         file: metadataResponse.file,
+        //         name: metadataResponse.name,
+        //         grade: metadataResponse.grade
+        //     }]
+        //     setFile(data);
+        // });
 
-    //     //Use dummy data
-    //     tester = [
-    //         {
-    //             file: "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252FClimby-32e99e66-edf1-4d5a-a4ee-57df69cad5c1/ImagePicker/30fd123d-ec54-4bfb-8d4e-7ff3995a6b76.mp4",
-    //             name: "Purps",
-    //             grade: "V3"
-    //         },
-    //         {
-    //             file: "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252FClimby-f9634f5b-366a-4190-9ef7-616f98661be2/ImagePicker/7b3b1cd4-9fe3-48b6-868d-a325f4a8a75e.mp4",
-    //             name: "Zoomy",
-    //             grade: "V6"
-    //         }
-    //     ]
-    //     setFile(tester)
+        //Use dummy data
+        tester = [
+            {
+                file: "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252FClimby-32e99e66-edf1-4d5a-a4ee-57df69cad5c1/ImagePicker/30fd123d-ec54-4bfb-8d4e-7ff3995a6b76.mp4",
+                name: "Purps",
+                grade: "V3"
+            },
+            {
+                file: "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252FClimby-f9634f5b-366a-4190-9ef7-616f98661be2/ImagePicker/7b3b1cd4-9fe3-48b6-868d-a325f4a8a75e.mp4",
+                name: "Zoomy",
+                grade: "V6"
+            },
+            {
+                file: "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252FClimby-91bb8297-2eb8-4def-b960-324105846193/ImagePicker/f3774d05-fbce-40e0-8564-aaf48aa9bcad.mp4",
+                name: "Bones",
+                grade: "V3"
+            }
+        ]
+        setFile(tester)
 
-    // }, []);
+    }, []);
 
     console.log(file);
 
